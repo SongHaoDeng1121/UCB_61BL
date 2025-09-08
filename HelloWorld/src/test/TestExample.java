f04f877a5c2d06ffd4b3397c7c3ead1cc3cc34a8
SONGHAO <sd1020ttt@gmail.com>
2025-09-08 16:19:58 +0800

    Clean wrongly added repos from index

---
+++ HelloWorld/src/test/TestExample.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestExample {
+    public static void main(String[] args) {
+        System.out.println("🚀 This is a standalone test from test/TestExample.java!");
+    }
+    
+}
